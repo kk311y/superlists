53ba8caa158638f0c555ded85d4786d5253a4001
kk311y <kkelly717@gmail.com>
2017-04-08 17:13:32 -0700

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,6 +7,7 @@ from django.utils.html import escape
 from lists.models import Item
 from lists.models import List
 from lists.views import home_page
+from lists.forms import ItemForm
 
 
 class NewListTest(TestCase):
@@ -43,6 +44,10 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
